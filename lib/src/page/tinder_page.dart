780db840b2b1569c2c06d41b1990ba7d4a371f61
Celso Junior <cjuniordev@gmail.com>
2022-06-28 14:17:23 -0300

    primeira atividade

--- lib/src/page/tinder_page.dart
+++ lib/src/page/tinder_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class TinderPage extends StatefulWidget {
@@ -15,7 +16,7 @@ class _TinderPageState extends State<TinderPage> {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 6),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 10),
           decoration: const BoxDecoration(
             gradient: LinearGradient(
                 colors: [Color(0xffe94976), Color(0xffee7462)],
@@ -39,31 +40,107 @@ class _TinderPageState extends State<TinderPage> {
                       )),
                 ],
               ),
+              const SizedBox(height: 120),
+              Image.asset('assets/images/logo_tinder.png', height: 40),
+              const SizedBox(height: 80),
+              RichText(
+                textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style:const  TextStyle(fontSize: 13),
+                    children: [
+                const TextSpan(
+                    text:
+                        'By tapping Create Account or Sing in, you agree to our '),
+                TextSpan(
+                  style: const TextStyle(decoration: TextDecoration.underline,
+                  fontWeight: FontWeight.bold,),
+                    text: 'Term',
+                    recognizer: TapGestureRecognizer()..onTap = () {}),
+                const TextSpan(text: '. Learn how we process your data in our '),
+                TextSpan(
+                    text: 'Privacy Policy ',
+                    style: const TextStyle(fontWeight: FontWeight.bold,decoration: TextDecoration.underline),
+                    recognizer: TapGestureRecognizer()..onTap = () {}),
+                const TextSpan(text: 'and '),
+                TextSpan(
+                    text: 'Cookies Policy',
+                    style: const TextStyle(fontWeight: FontWeight.bold,decoration: TextDecoration.underline),
+                    recognizer: TapGestureRecognizer()..onTap = () {}),
+                const TextSpan(text: '.')
+              ])),
+              const SizedBox(height: 30),
               OutlinedButton(
                 onPressed: () {},
-                child: Text('SING IN EITH APPLE',style: TextStyle(color: Color(0xfffeffff)),),
                 style: OutlinedButton.styleFrom(
-                  fixedSize: Size(MediaQuery.of(context).size.width,50),
+                    fixedSize: Size(MediaQuery.of(context).size.width, 45),
                     side:
                         const BorderSide(color: Color(0xfffeffff), width: 2.0),
                     shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.horizontal(
                             left: Radius.circular(50),
                             right: Radius.circular(50)))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('assets/images/logo_apple.png',height: 20,),
+                    const Spacer(),
+                    const Text(
+                      'SING IN EITH APPLE',
+                      style: TextStyle(color: Color(0xfffeffff)),
+                    ),
+                    const Spacer()
+                  ],
+                ),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 5),
               OutlinedButton(
                 onPressed: () {},
-                child: Text('SING IN EITH APPLE',style: TextStyle(color: Color(0xfffeffff)),),
                 style: OutlinedButton.styleFrom(
-                  fixedSize: Size(MediaQuery.of(context).size.width,50),
+                    fixedSize: Size(MediaQuery.of(context).size.width, 45),
                     side:
                         const BorderSide(color: Color(0xfffeffff), width: 2.0),
                     shape: const RoundedRectangleBorder(
                         borderRadius: BorderRadius.horizontal(
                             left: Radius.circular(50),
                             right: Radius.circular(50)))),
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/logo_facebook.png',height: 20,),
+                    const Spacer(),
+                    const Text('SING IN WITH FACEBOOK',
+                        style: TextStyle(color: Color(0xfffeffff))),
+                    const Spacer()
+                  ],
+                ),
               ),
+              const SizedBox(height: 5),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    fixedSize: Size(MediaQuery.of(context).size.width, 45),
+                    side:
+                        const BorderSide(color: Color(0xfffeffff), width: 2.0),
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(50),
+                            right: Radius.circular(50)))),
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/bubble.png',height: 20,),
+                    const Spacer(),
+                    const Text(
+                      'SING IN WITH PHONE NUMBER',
+                      style: TextStyle(color: Color(0xfffeffff)),
+                    ),
+                    const Spacer()
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text('Trouble Signing In?',
+                      style: TextStyle(color: Color(0xfffeffff))))
             ],
           ),
         ),
